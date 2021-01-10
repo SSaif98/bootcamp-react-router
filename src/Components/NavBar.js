3e2805a3b1f3ebd43ecd3de26c7356c2d9f8b705
SSaif98 <saifali7522@gmail.com>
2021-01-10 15:54:28 +0500

    .

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,6 +5,7 @@ import './Product.css';
 function NavBar() {
   return (
     <div className='mainNav'>
+       
         <Link to="/" className='navBar'>Home</Link>
         <Link to="/about" className='navBar'>About</Link>
         <Link to="/product" className='navBar'>Product</Link>
